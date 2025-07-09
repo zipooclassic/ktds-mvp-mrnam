08cd04484605b25af7998de8ef77e84855ec9f8b
zipooclassic <47814307+zipooclassic@users.noreply.github.com>
2025-07-10 08:39:54 +0900

    code_validator

---
+++ start_rag.sh
@@ -0,0 +1,75 @@
+
+# ./venv/Scripts/activate
+# uv pip install -r requirements.txt
+
+pip install annotated-types==0.7.0
+pip install anyio==4.9.0
+pip install azure-ai-vision-imageanalysis==1.0.0
+pip install azure-common==1.1.28
+pip install azure-core==1.35.0
+pip install azure-keyvault-secrets==4.10.0
+pip install azure-mgmt-compute==34.1.0
+pip install azure-mgmt-core==1.6.0
+pip install azure-mgmt-resource==24.0.0
+pip install azure-mgmt-storage==23.0.1
+pip install azure-search-documents==11.5.3
+pip install azure-storage-blob==12.25.1
+pip install blinker==1.9.0
+pip install certifi==2025.6.15
+pip install cffi==1.17.1
+pip install charset-normalizer==3.4.2
+pip install click==8.2.1
+pip install colorama==0.4.6
+pip install cryptography==45.0.5
+pip install distro==1.9.0
+pip install dotenv==0.9.9
+pip install filelock==3.18.0
+pip install flask==3.1.1
+pip install fsspec==2025.5.1
+pip install gitdb==4.0.12
+pip install gitpython==3.1.44
+pip install h11==0.16.0
+pip install httpcore==1.0.9
+pip install httpx==0.28.1
+pip install huggingface-hub==0.33.2
+pip install idna==3.10
+pip install isodate==0.7.2
+pip install itsdangerous==2.2.0
+pip install jinja2==3.1.6
+pip install jiter==0.10.0
+pip install joblib==1.5.1
+pip install markupsafe==3.0.2
+pip install mpmath==1.3.0
+pip install networkx==3.5
+pip install numpy==2.3.1
+pip install openai==1.93.1
+pip install packaging==25.0
+pip install pillow==11.3.0
+pip install pycparser==2.22
+pip install pydantic==2.11.7
+pip install pydantic-core==2.33.2
+pip install python-dotenv==1.1.1
+pip install pyyaml==6.0.2
+pip install regex==2024.11.6
+pip install requests==2.32.4
+pip install safetensors==0.5.3
+pip install scikit-learn==1.7.0
+pip install scipy==1.16.0
+pip install sentence-transformers==5.0.0
+pip install setuptools==80.9.0
+pip install six==1.17.0
+pip install smmap==5.0.2
+pip install sniffio==1.3.1
+pip install sympy==1.14.0
+pip install threadpoolctl==3.6.0
+pip install tokenizers==0.21.2
+pip install torch==2.7.1
+pip install tqdm==4.67.1
+pip install transformers==4.53.1
+pip install typing-extensions==4.14.1
+pip install typing-inspection==0.4.1
+pip install urllib3==2.5.0
+pip install werkzeug==3.1.3
+
+
+python code_validator.py
